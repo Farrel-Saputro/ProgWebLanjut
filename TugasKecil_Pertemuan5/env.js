b6b5e5e46a0558881f13038436898b409a559170
Farrel Saputro <93307670+Farrel-Saputro@users.noreply.github.com>
2023-10-02 10:49:51 +0700

    Add files via upload
    Tugas Kecil 2 Pertemuan 5

---
+++ TugasKecil_Pertemuan5/env.js
@@ -0,0 +1,14 @@
+// 1. Membuat Env Variable sesuai data yang diregister
+const cat_env = {
+  API_KEY: "",
+  endpoint: "https://api.thedogapi.com/",
+};
+
+const dog_env = {
+  API_KEY:
+    "live_vNU4vDxeMfL3A7uGD5ulo0KEniByesTYWu9Vbe6IQHlpvTTqstXwhwcI1tTCLKO9",
+  endpoint: "https://api.thedogapi.com/",
+};
+
+// 2. Export dog_env
+export { dog_env };
